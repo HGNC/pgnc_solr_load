d5825c825ae44bd02655565ec7df4fcc6a23fca5
Kris Gray <kag56@cam.ac.uk>
2025-03-04 18:15:27 +0000

    Enhance update script with success flag and completion check for solr-client

--- bin/update.sh
+++ bin/update.sh
@@ -2,3 +2,18 @@ echo "Transfer data to solr" 2>&1 | tee output.log
 python ./data-update/main.py --clear 2>&1 | tee -a output.log
 echo "Transfer data to dump file" 2>&1 | tee -a output.log
 python ./data-update/main.py --dump 2>&1 | tee -a output.log
+
+if [ $? -eq 0 ]; then
+  echo "Python script completed successfully."
+  touch /usr/src/app/output/done.flag  # Create success flag
+else
+  echo "Python script failed."
+fi
+
+echo "Waiting for solr-client to complete..."
+while [ ! -f /usr/src/app/output/solr-client-done.flag ]; do
+  sleep 5
+done
+
+echo "solr-client completed. Stopping python container..."
+exit 0  # Exits the container
