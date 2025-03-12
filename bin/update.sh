1911dc6748c5ab83682532dbbe20eea9b5690c85
Kris Gray <kag56@cam.ac.uk>
2025-03-12 16:00:59 +0000

    Remove success flag creation and solr-client wait logic from update script

--- bin/update.sh
+++ bin/update.sh
@@ -3,22 +3,3 @@ echo "Transfer data to solr" 2>&1 | tee output.log
 python ./data-update/main.py --clear 2>&1 | tee -a output.log
 echo "Transfer data to dump file" 2>&1 | tee -a output.log
 python ./data-update/main.py --dump 2>&1 | tee -a output.log
-
-if [ $? -eq 0 ]; then
-  echo "Python script completed successfully."
-  touch /usr/src/app/output/done.flag  # Create success flag
-else
-  echo "Python script failed."
-fi
-
-echo "Waiting for solr-client to complete..."
-while [ ! -f /usr/src/app/output/solr-client-done.flag ]; do
-  sleep 5
-done
-
-# while true; do
-#     sleep 180
-# done
-
-echo "solr-client completed. Stopping python container..."
-exit 0  # Exits the container
